c72e467ffef207e800371c3e8b9afd7b4438b698
Masab Ejaz <maabejaz@icloud.com>
2024-08-11 10:46:41 +0500

    updated person

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Shanza",
+                        name = "Laiba",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
